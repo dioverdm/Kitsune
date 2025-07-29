e90de6c2bb96df8e8649d3be25feb7dd4c8d1fc1
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-07-29 15:37:09 -0400

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -92,14 +92,14 @@ const NavBar = () => {
           className="flex items-center gap-1 cursor-pointer"
         >
           <Image src="/icon.png" alt="logo" width={70} height={70} />
-          <h1
+          {/*<h1
             className={cn([
               nightTokyo.className,
               "text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-200 to-pink-600 tracking-widest",
             ])}
           >
             Kitsunee
-          </h1>
+          </h1> */}
         </Link>
         <div className="hidden lg:flex items-center gap-10 ml-20">
           {menuItems.map((menu, idx) => (
