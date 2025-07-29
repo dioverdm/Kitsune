74a470b07de511edb2c22892124ff75fcab991f5
DioverDM <97247851+dioverdm@users.noreply.github.com>
2025-07-29 15:59:00 -0400

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DiscordLogoIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
+import { DiscordLogoIcon, TelegramLogoIcon, TiktokLogoIcon, YoutubeLogoIcon, WhatsappLogoIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 
 const Footer = () => {
@@ -7,18 +7,24 @@ const Footer = () => {
     <footer className="w-full bg-base-300 shadow-xl p-5 flex flex-col items-center space-y-5">
       <Image src="/icon.png" alt="logo" width="100" height="100" />
       <div className="flex space-x-5 items-center">
-        <a href="https://github.com/Dovakiin0/Kitsune" target="_blank">
-          <GitHubLogoIcon width="25" height="25" />
+        <a href="https://t.me/anios_tv" target="_blank">
+          <TelegramLogoIcon width="25" height="25" />
         </a>
-        <a href="https://discord.gg/6yAJ3XDHTt" target="_blank">
+        <a href="https://discord.gg/" target="_blank">
           <DiscordLogoIcon width="25" height="25" />
+        </a>
+        <a href="https://tiktok.com/@aniostv" target="_blank">
+          <TiktokLogoIcon width="25" target="_blank">
+        </a>
+        <a href="https://Youtube.com/@aniostv" target="_blank">
+          <YoutubeLogoIcon width="25" target="_blank">
+        </a>
+        <a href="https://discord.gg/" target="_blank">
+          <WhatsappLogoIcon width="25" target="_blank">
         </a>
       </div>
       <p className="text-sm text-gray-300">
-        Kitsune does not store any files on the server, we only link to the
-        media which is hosted on 3rd party services.
-      </p>
-      <p className="text-sm text-gray-300">&copy; Kitsune</p>
+        AniOS no almacena ningún archivo en el servidor, solo enlaza a los contenidos multimedia alojados en servicios de terceros.      <p className="text-sm text-gray-300">&copy; todos los derechos reservados a su desarrollador - DioverDM</p>
     </footer>
   );
 };
